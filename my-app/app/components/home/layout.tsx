48dd30fed68a79b3b5d7b223f2f8f3a6f593eeeb
Aadarsh <aadarsh9811594152@gmail.com>
2023-06-26 18:59:23 +0530

    initial commit

---
+++ my-app/app/components/home/layout.tsx
@@ -0,0 +1,10 @@
+const homelayout = (props: any) => {
+    return (
+        <div>
+        <h1>Homelayout</h1>
+        {props.children}
+        </div>
+    );
+    }
+    export default homelayout;
+
---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+const rootpage=()=>{
+  return(
+    <div>
+    <h1>Aadrash</h1>
+    </div>
+  )
+}
+export default rootpage;
+
+
